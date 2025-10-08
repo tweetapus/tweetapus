5880145e72ee46444de081259644063e011ea3e2
tiago <70700766+tiagozip@users.noreply.github.com>
2025-10-08 16:33:43 +0100

    a

--- v2/apps/web/app/routes/messages.$conversationId.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "@remix-run/node";
-
-export async function loader() {
-  return redirect("/messages");
-}
-
-export { default } from "./messages";
--- v2/packages/database/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./db";
-export { initializeDatabase } from "./init";
-export * from "./queries";
-export * from "./schema";
